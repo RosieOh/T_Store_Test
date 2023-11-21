552dda2b5af8854c966fee5c26beb6d11c1e687d
RosieOh <20172207@gm.hannam.ac.kr>
2023-11-21 09:50:19 +0900

    🌈fix : 도서검색 API 메인 페이지 검색 시 화면 패싱 오류 일단 제외 시키고 보류🌈

---
+++ src/main/java/com/shop/entity/BookBuy.java
@@ -0,0 +1,21 @@
+package com.shop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class BookBuy {
+    private int bbno;
+    private String title;
+    private String content;
+    private int author;
+    private Date regdate;
+    private int cnt;
+}
